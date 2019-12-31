3389da237da4388be0a30e186901c9bf337851fc
kg2000 <h_sj_nh@yeah.net>
2020-01-01 00:45:46 +0800

    提交src

---
+++ src/components/card.vue
@@ -0,0 +1,82 @@
+<template>
+
+		<div class="mui-content">
+			<div class="mui-card">
+				<div class="mui-card-content">
+					<div class="mui-card-content-inner">
+						年年有鱼
+					</div>
+				</div>
+			</div>
+			<!--<div class="mui-card">
+				<div class="mui-card-header">页眉</div>
+				<div class="mui-card-content">
+					<div class="mui-card-content-inner">
+						包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）
+					</div>
+				</div>
+				<div class="mui-card-footer">页脚</div>
+			</div>-->
+			
+			<div class="mui-card"><img src="../assets/images/niannianyouyu.png" alt="" width="100%"/>
+				
+				<div class="mui-card-content">
+					<div class="mui-card-content-inner">
+						<p>Posted on January 18, 2016</p>
+						<p style="color: #333;">“年年有鱼” 是“年年有余”的谐音，可谓中国传统吉祥祈福最具代表的语言之一，若用图画表示则可看作是传统吉祥符号。图中要有莲花或莲藕，还要有鱼，即“莲连有鱼”。代表生活富足，每年都有多余的财富及食粮！</p>
+					</div>
+				</div>
+				<div class="mui-card-footer">
+					<a class="mui-card-link">Like</a>
+					<a class="mui-card-link">Read more</a>
+				</div>
+			</div>
+			
+			
+			<!--<div class="mui-card">
+				<div class="mui-card-header mui-card-media">
+					<div class="mui-card-media-object mui-pull-left">
+						<img src="../assets/images/logo.png" width="34px" height="34px" />
+					</div>
+					<div class="mui-card-media-body">
+						小M
+						<p class="mui-ellipsis">发表于 6小时前</p>
+					</div>
+				</div>
+				<div class="mui-card-content mui-card-media" style="height:40vw;background-image:url(../assets/imagesurl(../assets/images/yuantiao.jpg)"></div>
+				<div class="mui-card-footer">
+					<a class="mui-card-link">Like</a>
+					<a class="mui-card-link">Comment</a>
+					<a class="mui-card-link">Read more</a>
+				</div>
+			</div>
+			
+			<div class="mui-card">
+				<div class="mui-card-header mui-card-media">
+					<img src="../assets/images/logo.png" />
+					<div class="mui-media-body">
+						小M
+						<p>发表于 2016-06-30 15:30</p>
+					</div>
+					<img class="mui-pull-left" src="../assets/images/logo.png" width="34px" height="34px" />
+					<h2>小M</h2>
+					<p>发表于 2016-06-30 15:30</p>
+				</div>
+				<div class="mui-card-content" >
+					<img src="../assets/images/yuantiao.jpg" alt="" width="100%"/>
+				</div>
+				<div class="mui-card-footer">
+					<a class="mui-card-link">Like</a>
+					<a class="mui-card-link">Comment</a>
+					<a class="mui-card-link">Read more</a>
+				</div>-->
+			</div>
+		</div>
+		
+</template>
+<script src="../assets/js/mui.min.js"></script>
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,185 @@
+<template>
+
+	<div class="mui-content">
+		<ul class="mui-table-view">
+			<li class="mui-table-view-cell">
+				新春快乐！
+				<div id="Gallery_Toggle" class="mui-switch">
+					<div class="mui-switch-handle"></div>
+				</div>
+			</li>
+		</ul>
+		<div id="Gallery" class="mui-slider" style="margin-top:15px;">
+			<div class="mui-slider-group">
+				<div class="mui-slider-item">
+					<ul class="mui-table-view mui-grid-view mui-grid-9">
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<router-link to='/card'>
+								<span class="svg-container">
+                                   <svg id="icon" aria-hidden="true">
+                                     <use xlink:href="#icon-chunjie-yu"></use>
+                                   </svg>
+                               </span>
+								<div class="mui-media-body">年年有鱼</div>
+							</router-link>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<router-link to='/HelloWorld'>
+								<span class="svg-container">
+                                   <svg id="icon" aria-hidden="true">
+                                     <use xlink:href="#icon-chunjie-yuanbao"></use>
+                                   </svg>
+                               </span>
+								<div class="mui-media-body">财源广进</div>
+							</router-link>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<router-link to='/index'>
+								<span class="svg-container">
+                                   <svg id="icon" aria-hidden="true">
+                                     <use xlink:href="#icon-chunjie-bianpao"></use>
+                                   </svg>
+                               </span>
+								<div class="mui-media-body">除旧迎新</div>
+							</router-link>
+						</li>
+						
+					</ul>
+				</div>
+				<div class="mui-slider-item">
+					<ul class="mui-table-view mui-grid-view mui-grid-9">
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-home"></span>
+								<div class="mui-media-body">Home</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
+								<div class="mui-media-body">Email</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-chatbubble"></span>
+								<div class="mui-media-body">Chat</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-location"></span>
+								<div class="mui-media-body">location</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-search"></span>
+								<div class="mui-media-body">Search</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-phone"></span>
+								<div class="mui-media-body">Phone</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-gear"></span>
+								<div class="mui-media-body">Setting</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-info"></span>
+								<div class="mui-media-body">about</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-more"></span>
+								<div class="mui-media-body">more</div>
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="mui-slider-item">
+					<ul class="mui-table-view mui-grid-view mui-grid-9">
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-home"></span>
+								<div class="mui-media-body">Home</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span>
+								<div class="mui-media-body">Email</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-chatbubble"></span>
+								<div class="mui-media-body">Chat</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-location"></span>
+								<div class="mui-media-body">location</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-search"></span>
+								<div class="mui-media-body">Search</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-phone"></span>
+								<div class="mui-media-body">Phone</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-gear"></span>
+								<div class="mui-media-body">Setting</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-info"></span>
+								<div class="mui-media-body">about</div>
+							</a>
+						</li>
+						<li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+							<a href="#">
+								<span class="mui-icon mui-icon-more"></span>
+								<div class="mui-media-body">more</div>
+							</a>
+						</li>
+					</ul>
+				</div>
+
+			</div>
+			<div class="mui-slider-indicator">
+				<div class="mui-indicator mui-active"></div>
+				<div class="mui-indicator"></div>
+				<div class="mui-indicator"></div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style>
+	.icon {
+		width: 1em;
+		height: 1em;
+		vertical-align: -0.15em;
+		fill: currentColor;
+		overflow: hidden;
+	}
+</style>
+<link rel="stylesheet" href="./iconfont.css">
